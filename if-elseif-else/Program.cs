ad1b8ae41fd613ea75811cf16cfe67c425a3a85c
Ullasong <21codewar@gmail.com>
2022-05-14 16:56:06 +0900

    2-exercise-if

---
+++ if-elseif-else/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random dice = new Random();
+int firstRoll = dice.Next(1, 7);
+int secondRoll = dice.Next(1, 7);
+int thirdRoll = dice.Next(1, 7);
+int total = firstRoll + secondRoll + thirdRoll;
+
+if ((firstRoll == secondRoll) || (secondRoll == thirdRoll) || (thirdRoll == firstRoll))
+{
+    total += 2;
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+}
+if((firstRoll == secondRoll && secondRoll == thirdRoll && thirdRoll == firstRoll))
+{
+    total += 6;
+    Console.WriteLine("You rolled triple! +6 bonus to total!");
+}
+if (total >= 15)
+{
+  Console.WriteLine("You Win");
+}
+else
+{
+    Console.WriteLine("You Lose");
+} 
