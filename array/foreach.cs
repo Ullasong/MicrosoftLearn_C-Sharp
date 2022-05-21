b0f1940c025cbc21665028891d8dc9d6a8c57b28
Ullasong <21codewar@gmail.com>
2022-05-21 18:56:15 +0900

    foreach

---
+++ array/foreach.cs
@@ -0,0 +1,9 @@
+﻿int[] inventory = { 42, 23, 18, 34};
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory) {
+    sum += items;
+    bin ++;
+    Console.WriteLine($"{bin}번 보관함의 완제품 수 : {items} (누계 : {sum})");
+}
